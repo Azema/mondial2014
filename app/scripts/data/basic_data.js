bff165b43636a4a0702a910ce0838707c56b0ecb
Meichel Cyrille <cmeichel@digitaleo.com>
2014-01-22 14:26:29 +0100

    data model for countries

---
+++ app/scripts/data/basic_data.js
@@ -0,0 +1,239 @@
+var countries = {
+	"algeria": {
+		"name": "Algeria",
+		"flagIcon" : "images/flags/icons/algeria.png",
+		"flag" : "images/flags/mediums/algeria.png"
+	},
+	"argentina": {
+		"name": "Argentia",
+		"flagIcon" : "images/flags/icons/argentina.png",
+		"flag" : "images/flags/mediums/argentina.png"
+	},
+	"australia": {
+		"name": "Australia",
+		"flagIcon" : "images/flags/icons/australia.png",
+		"flag" : "images/flags/mediums/australia.png"
+	},
+	"belgium": {
+		"name": "Belgium",
+		"flagIcon" : "images/flags/icons/belgium.png",
+		"flag" : "images/flags/mediums/belgium.png"
+	},
+	"bosnia_and_herzegovina": {
+		"name": "Bosnia and Herzegovina",
+		"flagIcon" : "images/flags/icons/bosnia_and_herzegovina.png",
+		"flag" : "images/flags/mediums/bosnia_and_herzegovina.png"
+	},
+	"brazil": {
+		"name": "Brazil",
+		"flagIcon" : "images/flags/icons/brazil.png",
+		"flag" : "images/flags/mediums/icons/brazil.png"
+	},
+	"cameroon": {
+		"name": "Cameroon",
+		"flagIcon" : "images/flags/icons/cameroon.png",
+		"flag" : "images/flags/mediums/cameroon.png"
+	},
+	"chile": {
+		"name": "Chile",
+		"flagIcon" : "images/flags/icons/chile.png",
+		"flag" : "images/flags/mediums/chile.png"
+	},
+	"colombia": {
+		"name": "Colombia",
+		"flagIcon" : "images/flags/icons/colombia.png",
+		"flag" : "images/flags/mediums/colombia.png"
+	},
+	"costa_rica": {
+		"name": "Costa Rica",
+		"flagIcon" : "images/flags/icons/costa_rica.png",
+		"flag" : "images/flags/mediums/costa_rica.png"
+	},
+	"cote_d_ivoire": {
+		"name": "Côte d'Ivoire",
+		"flagIcon" : "images/flags/icons/cote_d_ivoire.png",
+		"flag" : "images/flags/mediums/cote_d_ivoire.png"
+	},
+	"croatia": {
+		"name": "Croatia",
+		"flagIcon" : "images/flags/icons/croatia.png",
+		"flag" : "images/flags/mediums/croatia.png"
+	},
+	"ecuador": {
+		"name": "Ecuador",
+		"flagIcon" : "images/flags/icons/ecuador.png",
+		"flag" : "images/flags/mediums/ecuador.png"
+	},
+	"england": {
+		"name": "England",
+		"flagIcon" : "images/flags/icons/england.png",
+		"flag" : "images/flags/mediums/england.png"
+	},
+	"france": {
+		"name": "France",
+		"flagIcon" : "images/flags/icons/france.png",
+		"flag" : "images/flags/mediums/france.png"
+	},
+	"germany": {
+		"name": "Germany",
+		"flagIcon" : "images/flags/icons/germany.png",
+		"flag" : "images/flags/mediums/germany.png"
+	},
+	"ghana": {
+		"name": "ghana",
+		"flagIcon" : "images/flags/icons/ghana.png",
+		"flag" : "images/flags/mediums/ghana.png"
+	},
+	"greece": {
+		"name": "Greece",
+		"flagIcon" : "images/flags/icons/greece.png",
+		"flag" : "images/flags/mediums/greece.png"
+	},
+	"honduras": {
+		"name": "Honduras",
+		"flagIcon" : "images/flags/icons/honduras.png",
+		"flag" : "images/flags/mediums/honduras.png"
+	},
+	"iran": {
+		"name": "Iran",
+		"flagIcon" : "images/flags/icons/iran.png",
+		"flag" : "images/flags/mediums/iran.png"
+	},
+	"italy": {
+		"name": "Italy",
+		"flagIcon" : "images/flags/icons/italy.png",
+		"flag" : "images/flags/mediums/italy.png"
+	},
+	"japan": {
+		"name": "Japan",
+		"flagIcon" : "images/flags/icons/japan.png",
+		"flag" : "images/flags/mediums/japan.png"
+	},
+	"mexico": {
+		"name": "Mexico",
+		"flagIcon" : "images/flags/icons/mexico.png",
+		"flag" : "images/flags/mediums/mexico.png"
+	},
+	"netherlands": {
+		"name": "Netherlands",
+		"flagIcon" : "images/flags/icons/netherlands.png",
+		"flag" : "images/flags/mediums/netherlands.png"
+	},
+	"nigeria": {
+		"name": "Nigeria",
+		"flagIcon" : "images/flags/icons/nigeria.png",
+		"flag" : "images/flags/mediums/nigeria.png"
+	},
+	"portugal": {
+		"name": "Portugal",
+		"flagIcon" : "images/flags/icons/portugal.png",
+		"flag" : "images/flags/mediums/portugal.png"
+	},
+	"russia": {
+		"name": "Russia",
+		"flagIcon" : "images/flags/icons/russia.png",
+		"flag" : "images/flags/mediums/russia.png"
+	},
+	"south_korea": {
+		"name": "South Korea",
+		"flagIcon" : "images/flags/icons/south_korea.png",
+		"flag" : "images/flags/mediums/south_korea.png"
+	},
+	"spain": {
+		"name": "Spain",
+		"flagIcon" : "images/flags/icons/spain.png",
+		"flag" : "images/flags/mediums/spain.png"
+	},
+	"switzerland": {
+		"name": "Switzerland",
+		"flagIcon" : "images/flags/icons/switzerland.png",
+		"flag" : "images/flags/mediums/switzerland.png"
+	},
+	"united_states": {
+		"name": "United States",
+		"flagIcon" : "images/flags/icons/united_states.png",
+		"flag" : "images/flags/mediums/united_states.png"
+	},
+	"uruguay": {
+		"name": "Uruguay",
+		"flagIcon" : "images/flags/icons/uruguay.png",
+		"flag" : "images/flags/mediums/uruguay.png"
+	}
+}
+
+
+
+var groups = {
+	"a":{
+		"name": "Group A",
+		"countries" : [
+			countries.brazil,
+			countries.croatia,
+			countries.mexico,
+			countries.cameroon
+		]
+	},
+	"b":{
+		"name": "Group B",
+		"countries" : [
+			countries.spain,
+			countries.netherlands,
+			countries.chile,
+			countries.australia
+		]
+	},
+	"c":{
+		"name": "Group C",
+		"countries" : [
+			countries.colombia,
+			countries.greece,
+			countries.cote_d_ivoire,
+			countries.japan
+		]
+	},
+	"d":{
+		"name": "Group D",
+		"countries" : [
+			countries.uruguay,
+			countries.costa_rica,
+			countries.england,
+			countries.italy
+		]
+	},
+	"e":{
+		"name": "Group E",
+		"countries" : [
+			countries.switzerland,
+			countries.ecuador,
+			countries.france,
+			countries.honduras
+		]
+	},
+	"f":{
+		"name": "Group F",
+		"countries" : [
+			countries.argentina,
+			countries.bosnia_and_herzegovina,
+			countries.iran,
+			countries.nigeria
+		]
+	},
+	"g":{
+		"name": "Group G",
+		"countries" : [
+			countries.germany,
+			countries.portugal,
+			countries.ghana,
+			countries.united_states
+		]
+	},
+	"h":{
+		"name": "Group H",
+		"countries" : [
+			countries.belgium,
+			countries.algeria,
+			countries.russia,
+			countries.south_korea
+		]
+	},
+};
